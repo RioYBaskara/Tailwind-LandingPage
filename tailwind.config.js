2815c18f117bb4812dae7aa8e4a7700816b07ed3
Rio Yudha Baskara <156874101+RioYBaskara@users.noreply.github.com>
2024-11-14 14:15:48 +0700

    color fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,26 +4,17 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        // primary60%
-        ivory: "#FFFFF0",
-        "antique-white": "#FAEBD7",
-        // secondary30%
-        "muted-sand": "#C2B280",
-        "warm-taupe": "#A39277",
-        // accent10%
-        "deep-olive-green": "#556B2F",
-        "burnt-sienna": "#8B4513",
-        "burnt-orange": "#C04B1E",
-        // testing
-        "terra-cotta": "#E27D60", // Soft earthy orange
-        "deep-burgundy": "#800020", // Elegant dark red
-        "olive-green": "#808000", // Natural green
-        "golden-ochre": "#C88A4E", // Warm golden yellow
-        "charcoal-gray": "#36454F", // Deep gray for text
+        base: "#F6F6EC",
+
+        second: "#EFEDDF",
+
+        accent: "#C2B280",
+        accent2: "#800020",
+
+        hover: "#4d0013",
       },
       fontFamily: {
-        serif: ['"Garamond"', "serif"],
-        sans: ['"Open Sans"', "sans-serif"],
+        Lora: ["Lora", "serif"],
       },
     },
   },
