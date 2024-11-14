b2f3272e64ca19070e640f524ce9d17623b07586
Rio Yudha Baskara <156874101+RioYBaskara@users.noreply.github.com>
2024-11-14 12:36:14 +0700

    rancangan website

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./index.html", "./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // primary60%
+        ivory: "#FFFFF0",
+        "antique-white": "#FAEBD7",
+        // secondary30%
+        "muted-sand": "#C2B280",
+        "warm-taupe": "#A39277",
+        // accent10%
+        "deep-olive-green": "#556B2F",
+        "burnt-sienna": "#8B4513",
+        "burnt-orange": "#C04B1E",
+        // testing
+        "terra-cotta": "#E27D60", // Soft earthy orange
+        "deep-burgundy": "#800020", // Elegant dark red
+        "olive-green": "#808000", // Natural green
+        "golden-ochre": "#C88A4E", // Warm golden yellow
+        "charcoal-gray": "#36454F", // Deep gray for text
+      },
+      fontFamily: {
+        serif: ['"Garamond"', "serif"],
+        sans: ['"Open Sans"', "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
