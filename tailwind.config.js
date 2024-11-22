92abd92fd91fec2cecf46ff99b51f2563b4fb51e
Rio Yudha Baskara <156874101+RioYBaskara@users.noreply.github.com>
2024-11-22 08:32:08 +0700

    mobile, nav

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,36 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{html,js}"],
   theme: {
+    fontSize: {
+      head1: ["47.78px", "56px"],
+      head2: ["39.81px", "48px"],
+      head3: ["33.18px", "40px"],
+      head4: ["27.65px", "32px"],
+      head5: ["23.04px", "32px"],
+      head6: ["19.2px", "24px"],
+
+      prsmall: [
+        "13.33px",
+        "16px",
+        {
+          fontWeight: "400",
+        },
+      ],
+      prregular: [
+        "16px",
+        "16px",
+        {
+          fontWeight: "400",
+        },
+      ],
+      prlarge: [
+        "19.2px",
+        "24px",
+        {
+          fontWeight: "400",
+        },
+      ],
+    },
     extend: {
       colors: {
         base: "#F6F6EC",
@@ -9,7 +39,7 @@ module.exports = {
         second: "#EFEDDF",
 
         accent: "#C2B280",
-        accent2: "#800020",
+        accent2: "#071531",
 
         hover: "#4d0013",
       },
