1e1ee90f97952d7215c9f358fae01fe0cc63b6b3
Rio Yudha Baskara <156874101+RioYBaskara@users.noreply.github.com>
2024-11-25 12:46:51 +0700

    nav tablet md view selesai

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,6 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{html,js}"],
   theme: {
-    backgroundImage: {
-      "hero-foto": "url('/assets/img/Hero.jpg')",
-    },
     fontSize: {
       head1: ["47.78px", "56px"],
       head2: ["39.81px", "48px"],
